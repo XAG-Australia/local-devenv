663eae374799860512a47bacdf85fd3c4c17a96d
XAG-Australia <david@thesydney.agency>
2023-07-03 18:14:25 +1000

    Updated Homepage Source

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -9,8 +9,8 @@ $techup_banner_button_link1 = get_theme_mod( 'techup_banner_button_link1','');
 if($techup_enable_banner_section==true ) {
 ?>  
 <!-- ======= Hero Section ======= -->
-<section class="hero-sec beg-img" style="background-image:url(<?php if($techup_banner_image) { echo esc_url($techup_banner_image); } else { echo esc_url(get_header_image()); } ?>); opacity: 0.5; height: 50vh;">
-<video autoplay muted loop id="myVideo" style="position: fixed; min-width: 100%; max-height: 50vh; right: 0; bottom: 0; object-fit: cover;">
+<section class="hero-sec beg-img" style="background-image:url(<?php if($techup_banner_image) { echo esc_url($techup_banner_image); } else { echo esc_url(get_header_image()); } ?>); height: 50vh; background-opacity: 0.1;">
+<video autoplay muted loop id="myVideo" style="position: fixed; min-width: 100%; max-height: 50vh; right: 0; top: 0; object-fit: cover;">
 <source src="http://localhost:8080/wp-content/uploads/2023/07/file.mp4" type="video/mp4">      
 <div class="container">
      
