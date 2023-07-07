0b0be6eb1af09d14ca4b88b76ff6c345fe1ef0a2
XAG-Australia <david@thesydney.agency>
2023-07-07 15:39:26 +1000

    Installation of slider

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -18,9 +18,9 @@ if($techup_enable_banner_section==true ) { ?>
 
 <section id="slidr-div" class="hero-sec beg-img">
 
-<script type="text/javascript" src="/slidr-master/slidr-master/slidr.min.js">
+<script type="text/javascript" src="/slidr-master/slidr.min.js">
 
-slidr.create('slidr-id', {
+slidr.create('slidr-div', {
   after: function(e) { console.log('in: ' + e.in.slidr); },
   before: function(e) { console.log('out: ' + e.out.slidr); },
   breadcrumbs: true,
