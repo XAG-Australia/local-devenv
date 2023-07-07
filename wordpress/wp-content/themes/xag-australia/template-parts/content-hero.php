8bbc98878ee88b00e5d4c4f9f9be2a3512507c6c
XAG-Australia <david@thesydney.agency>
2023-07-07 16:36:43 +1000

    Installing Slider

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -8,10 +8,31 @@ $techup_banner_button_link1 = get_theme_mod( 'techup_banner_button_link1','');
       
 if($techup_enable_banner_section==true ) { ?>
 
+<script type="text/javascript" src="/slidr-master/slidr.min.js"></script>
 
+<div id="slidr-div" style="display: block">
+  <div data-slidr="one">apple</div>
+  <div data-slidr="two">banana</div>
+  <div data-slidr="three">coconut</div>
+</div>
 
 <div id="content"></div>
-
+<script>
+slidr.create('slidr-id', {
+  after: function(e) { console.log('in: ' + e.in.slidr); },
+  before: function(e) { console.log('out: ' + e.out.slidr); },
+  breadcrumbs: true,
+  controls: 'corner',
+  direction: 'vertical',
+  fade: false,
+  keyboard: true,
+  overflow: true,
+  theme: '#222',
+  timing: { 'cube': '0.5s ease-in' },
+  touch: true,
+  transition: 'cube'
+}).start();
+<script>
 <?php
 }
 ?>
