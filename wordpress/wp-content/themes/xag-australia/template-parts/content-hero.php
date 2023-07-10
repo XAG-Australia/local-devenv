201e65bfa0fda233bcbb162d2a1112e2a5161f95
XAG-Australia <david@thesydney.agency>
2023-07-10 11:36:55 +1000

    Update content-hero.php

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -137,6 +137,22 @@ aside[id="{slidr-id}-breadcrumbs"] .slidr-breadcrumbs li.active {
   </div>
 </div>
 
+<aside id="{slidr-id}-breadcrumbs">
+  <ul class="slidr-breadcrumbs">
+    <li></li>
+    <li class="normal"></li>
+    <li class="normal active"></li>
+  </ul>
+  ...
+</aside>
+
+<aside id="{slidr-id}-control">
+  <div class="slidr-control up"></div>
+  <div class="slidr-control down"></div>
+  <div class="slidr-control left"></div>
+  <div class="slidr-control right"></div>
+</aside>
+
 <div id="content"></div>
 <script>
 slidr.create('slidr-div', {
