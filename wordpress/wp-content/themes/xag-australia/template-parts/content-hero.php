c88a9710cafc76ebb9258471ece4d40841cf5611
XAG-Australia <david@thesydney.agency>
2023-07-10 12:02:33 +1000

    Update content-hero.php

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -46,8 +46,18 @@ aside[id="{slidr-id}-control"].border .slidr-control.left:hover {
   opacity: 1;
 }
 
+// Hide a single arrow within a single controller.
+aside[id="{slidr-id}-control"] .slidr-control.right:after {
+  border-color: transparent !important;
+}
+
+// Hide all arrows within a single controller.
+aside[id="{slidr-id}-control"] .slidr-control:after {
+  border-color: transparent !important;
+}
+
 // Hide all Slidr arrows.
-aside[id*="-control"].border .slidr-control after {
+aside[id*="-control"] .slidr-control:after {
   border-color: transparent !important;   
 }
 </style>
