d45bffb4c4e261b789c3f4861358463cee4b851c
XAG-Australia <david@thesydney.agency>
2023-07-12 11:34:48 +1000

    Update content-hero.php

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -60,7 +60,7 @@ if($techup_enable_banner_section==true ) { ?>
 
 
 	aside[id="slidr-div-control"].border .slidr-control.left:hover {
-		
+		opacity: 1 !important;	
 	background: url('http://localhost:8080/wp-content/uploads/2023/07/left-arrow-1.png') no-repeat rgb(0 0 0 /0.25);
 	background-size: 25px;
 	background-position: center;
