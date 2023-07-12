e6e182057abada260ee0cd19896e61db3bbb40db
XAG-Australia <david@thesydney.agency>
2023-07-12 15:05:31 +1000

    Update content-features.php

--- wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
@@ -79,13 +79,13 @@ if($techup_enable_features_section==true ) {
     </div>
   </div>
   <div class="feature-sec" style="height: calc(40vh + 80px);">
-      <div class="featured-1" style="height: calc(40vh + 80px); width: 33vw; background-color: #000000;">
+      <div class="featured-1" style="height: calc(40vh + 80px); width: 33vw; background-color: #000000; float: left;">
     
     </div>
-      <div class="featured-2" style="height: calc(40vh + 80px); width: 33vw; background-color: #999999;">
+      <div class="featured-2" style="height: calc(40vh + 80px); width: 33vw; background-color: #999999; float: left;">
     
     </div>
-      <div class="featured-3" style="height: calc(40vh + 80px); width: 33vw; background-color: #333333;">
+      <div class="featured-3" style="height: calc(40vh + 80px); width: 33vw; background-color: #333333; float: left;">
     
     </div>
   </div>
