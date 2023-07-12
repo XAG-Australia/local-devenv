6d0011ad38977f28f74cd9f8192c6a14aa6c3cac
XAG-Australia <david@thesydney.agency>
2023-07-12 12:48:16 +1000

    Resizing main menu

--- wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
@@ -16,7 +16,7 @@ if($techup_enable_features_section==true ) {
         $techup_features_query = new WP_Query( $techup_features_args );
 ?>
 <div class="feature-sec buco" style="height: 60vh;">
-    <h2 class="section-title" style="font-size: 38px; text-shadow: 3px 3px 40px rgba(0,0,0,0.75); text-align: center; margin-top: 80px; margin-bottom: 50px;">Explore Australian Agriculture Technology.</h2>
+    <h2 class="section-title" style="font-size: 28px; text-shadow: 3px 3px 40px rgba(0,0,0,0.75); text-align: center; margin-top: 80px; margin-bottom: 50px;">Explore Australian Agriculture Technology.</h2>
     <div class="container-fluid" style="width: 1480px; padding-left: 0px; padding-right: 0px;">
       <div class="row">
 		<?php
