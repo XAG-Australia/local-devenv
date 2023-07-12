2891e450db32b2bcb8b8c0c68b1f8e7934073646
XAG-Australia <david@thesydney.agency>
2023-07-12 11:13:31 +1000

    Update content-hero.php

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -144,7 +144,7 @@ if($techup_enable_banner_section==true ) { ?>
 				<div class="container">
 					<div class="row">
 						<div class="content-sec">
-							<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-s.png" width="120">
+							<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-s.png" width="160">
 							<h2 class="section-title" style="font-size: 38px; text-shadow: 3px 3px 20px rgba(0,0,0,0.75);"><?php echo esc_html($techup_banner_title); ?>
 							</h2>
 							<?php if($techup_banner_button_label1) :?>
@@ -168,7 +168,7 @@ if($techup_enable_banner_section==true ) { ?>
 				<div class="container">
 					<div class="row">
 						<div class="content-sec">
-						<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-s.png" width="120">
+						<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-s.png" width="160">
 							<h2 class="section-title" style="font-size: 38px; text-shadow: 3px 3px 20px rgba(0,0,0,0.75);">Australian Agricultural Drone Technology.</h2>
 
 
@@ -189,7 +189,7 @@ if($techup_enable_banner_section==true ) { ?>
 				<div class="container">
 					<div class="row">
 						<div class="content-sec">
-						<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-s.png" width="120">
+						<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-s.png" width="160">
 							<h2 class="section-title" style="font-size: 28px; text-shadow: 3px 3px 20px rgba(0,0,0,0.75);">Australian Aerial Spraying, Seeding & Surveying Drones.</h2>
 
 
