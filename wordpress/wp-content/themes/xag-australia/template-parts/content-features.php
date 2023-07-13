ae9009aab99190effbf6b725e922c505c6005e44
XAG-Australia <david@thesydney.agency>
2023-07-13 14:02:40 +1000

    Update content-features.php

--- wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
@@ -17,7 +17,7 @@ if($techup_enable_features_section==true ) {
 ?>
 <div class="feature-sec buco" style="height: 87vh; margin-top: 100px; padding: 0px;">
     <h2 class="section-title" style="font-size: 28px; text-align: center; color: #d0d0d0; font-weight: 400 !important; font-family: 'Roboto Condensed', sans-serif; letter-spacing: 0px; margin-bottom: 50px; margin-top: 50px;">XAG Australia specialises in providing aerial, and unmanned ground <b style="color:#c01920; ">drone technology.</b></h2>
-    <div class="container-fluid" style="width: calc(60vw + 200px); padding-left: 0px; padding-right: 0px;">
+    <div class="container-fluid" style="width: calc(60vw + 150x); padding-left: 0px; padding-right: 0px;">
       <div class="row" style="width: 100%; margin-left: auto; margin-right: auto;">
 		<?php
 		$count = 0;
@@ -42,7 +42,7 @@ if($techup_enable_features_section==true ) {
 			      </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 col-sm-12" style="max-width: 20vw; margin-right: 50px;">
+        <div class="col-lg-4 col-md-6 col-sm-12" style="max-width: 20vw;">
           <div class="feature-box feature-box-one feature-box-3" style="width: 20vw; background-color: #ffffff !important; max-width: 480px; height: 460px;  border-radius: 20px; ">
             <div class="icon" style="background: url('http://localhost:8080/wp-content/uploads/2023/07/service-3.png') no-repeat #ffffff; background-size: 120px; background-position: center; margin-bottom: 30px;"></div>
             <div class="content">
