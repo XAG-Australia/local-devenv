31400007eafb80bb5fb456173b906e2ed2ffba9e
XAG-Australia <david@thesydney.agency>
2023-07-18 17:20:34 +1000

    Update footer-hooks.php

--- wordpress/wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
+++ wordpress/wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
@@ -64,7 +64,6 @@ if( ! function_exists( 'techup_footer_start' ) ):
 endif; 
 if( ! function_exists( 'techup_footer_sidebar' ) ):
 	function techup_footer_sidebar(){ ?>
-<div class="footer-section" style="height: 100%; width: 100%">
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3309.6632911974166!2d151.2197601765195!3d-33.949787273195724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12b16a9e88eaf9%3A0x26cb380defb0dc04!2sXAG%20Australia!5e0!3m2!1sen!2sau!4v1689663545448!5m2!1sen!2sau" style="border:0; width: 100vw; height: 100vh; z-index: 1;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 	    <div class="foot-top" style="height: 100%; z-index: 999;">
             <div class="container-left" style="float: left; width: 50%; height: 100%;">
@@ -90,7 +89,6 @@ if( ! function_exists( 'techup_footer_sidebar' ) ):
                     <input type="submit" value="Submit">
             </form>
             </div>
-    </div>
 	<?php }
 endif; 
 if( ! function_exists( 'techup_footer_site_info' ) ):
