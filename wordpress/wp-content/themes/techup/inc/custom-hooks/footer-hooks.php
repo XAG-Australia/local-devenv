551893b13705d64dd2144a8f114230b2ff4125f9
XAG-Australia <david@thesydney.agency>
2023-07-18 17:01:24 +1000

    adding gmap

--- wordpress/wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
+++ wordpress/wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
@@ -44,6 +44,10 @@ input[type=submit]:hover {
     margin-top: 20px;
     margin-bottom: 20px;
 }
+#map {
+    height: 400px; /* The height is 400 pixels */
+    width: 100%; /* The width is the width of the web page */
+}
 </style>
 <?php
 /**
@@ -66,7 +70,41 @@ if( ! function_exists( 'techup_footer_sidebar' ) ):
 	function techup_footer_sidebar(){ ?>
 	    <div class="foot-top" style="height: 100%;">
             <div class="container-left" style="float: left; width: 50%; height: 100%;">
-            
+            <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+            <!--The div element for the map --><script>
+            // Initialize and add the map
+                let map;
+
+                async function initMap() {
+                // The location of Uluru
+                const position = { lat: -33.9497873, lng: 151.2197602 };
+                // Request needed libraries.
+                //@ts-ignore
+                const { Map } = await google.maps.importLibrary("maps");
+                const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+                // The map, centered at Uluru
+                map = new Map(document.getElementById("map"), {
+                    zoom: 4,
+                    center: position,
+                    mapId: "DEMO_MAP_ID",
+                });
+
+                // The marker, positioned at Uluru
+                const marker = new AdvancedMarkerElement({
+                    map: map,
+                    position: position,
+                    title: "Uluru",
+                });
+                }
+
+                initMap();
+                </script>
+                <div id="map"></div>
+
+            <!-- prettier-ignore -->
+                        <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+                    ({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "beta"});</script>
             </div>
             <div class="container-right" style="float: left; width: 50%; height: 100%;">
             <form action="/action_page.php">
