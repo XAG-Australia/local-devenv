198aafa9cc28189935d67db9777b16b8f3891764
XAG-Australia <david@thesydney.agency>
2023-07-18 16:03:12 +1000

    adding contact form

--- wordpress/wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
+++ wordpress/wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
@@ -1,3 +1,34 @@
+<style>
+    input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #04AA6D;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container-left {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
 <?php
 /**
  * Managed the custom functions and hooks for footer section of the theme.
@@ -22,7 +53,25 @@ if( ! function_exists( 'techup_footer_sidebar' ) ):
             
             </div>
             <div class="container-right" style="float: left; width: 50%; height: 100%;">
-            
+            <form action="/action_page.php">
+                    <label for="fname">First Name</label>
+                    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+                    <label for="lname">Last Name</label>
+                    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+                    <label for="country">Country</label>
+                    <select id="country" name="country">
+                    <option value="australia">Australia</option>
+                    <option value="canada">Canada</option>
+                    <option value="usa">USA</option>
+                    </select>
+
+                    <label for="subject">Subject</label>
+                    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+                    <input type="submit" value="Submit">
+            </form>
             </div>
 	<?php }
 endif; 
