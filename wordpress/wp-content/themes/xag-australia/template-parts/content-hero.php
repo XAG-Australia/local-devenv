64175ac0cafb123e5a371e89680db737969cfdd1
XAG-Australia <david@thesydney.agency>
2023-07-19 16:35:59 +1000

    Update content-hero.php

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -165,7 +165,7 @@ if($techup_enable_banner_section==true ) { ?>
 
 		<div data-slidr="one" style="min-width: 100vw; min-height: 50vh;">
 			<section class="hero-sec" style="background-image: url('http://localhost:8080/wp-content/uploads/2023/07/slide-1X.jpg'); background-attachment: fixed; background-size: contain; background-position: center; width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important;">
-				<div class="container" style="padding-left: 0px; padding-right: 0px; margin-left: unset; margin-right: unset; width: 35% !important; height: 100% !important; background: #232323; border-radius: 0px 200px 200px 0px; max-width: 1280px !important;">
+				<div class="container" style="padding-left: 0px; padding-right: 0px; margin-left: unset; margin-right: unset; width: 35% !important; height: 100% !important; background: #232323; border-radius: 0px 500px 500px 0px; max-width: 1280px !important;">
 					<div class="row">
 						<div class="content-sec" style="right: 50px;">
 							<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-1.png" width="160">
@@ -189,7 +189,7 @@ if($techup_enable_banner_section==true ) { ?>
 
 		<div data-slidr="two" style="min-width: 100vw; min-height: 50vh;">
 		<section class="hero-sec" style="background-image: url('http://localhost:8080/wp-content/uploads/2023/07/slide-2X.jpg'); background-attachment: fixed; background-size: contain; background-position: center; width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important;">
-				<div class="container" style="padding-left: 0px; padding-right: 0px; margin-left: unset; margin-right: unset; width: 35% !important; height: 100% !important; background: #232323; border-radius: 0px 200px 200px 0px; max-width: 1280px !important;">
+				<div class="container" style="padding-left: 0px; padding-right: 0px; margin-left: unset; margin-right: unset; width: 35% !important; height: 100% !important; background: #232323; border-radius: 0px 500px 500px 0px; max-width: 1280px !important;">
 					<div class="row">
 						<div class="content-sec" style="right: 50px;">
 						<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-1.png" width="160">
@@ -211,7 +211,7 @@ if($techup_enable_banner_section==true ) { ?>
 		<div data-slidr="three" style="min-width: 100vw; min-height: 50vh;">
 			<section class="hero-sec" style=" background-image: url('http://localhost:8080/wp-content/uploads/2023/07/slide-3X.jpg'); background-attachment: fixed; background-size: contain
 			; background-position: center; width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important;">
-				<div class="container" style="padding-left: 0px; padding-right: 0px; margin-left: unset; margin-right: unset; width: 35% !important; height: 100% !important; background: #232323; border-radius: 0px 200px 200px 0px; max-width: 1280px !important;">
+				<div class="container" style="padding-left: 0px; padding-right: 0px; margin-left: unset; margin-right: unset; width: 35% !important; height: 100% !important; background: #232323; border-radius: 0px 500px 500px 0px; max-width: 1280px !important;">
 					<div class="row">
 						<div class="content-sec" style="right: 50px;">
 						<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-1.png" width="160">
