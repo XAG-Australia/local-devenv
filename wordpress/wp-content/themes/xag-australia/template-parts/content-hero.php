6487c80bbbb2fcae505fa95770ea7cfcd0493860
XAG-Australia <david@thesydney.agency>
2023-07-19 14:41:11 +1000

    Update content-hero.php

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -164,10 +164,10 @@ if($techup_enable_banner_section==true ) { ?>
 
 
 		<div data-slidr="one" style="min-width: 100vw; min-height: 50vh;">
-			<section class="hero-sec" style="background-image: url('http://localhost:8080/wp-content/uploads/2023/07/slide-3.jpg'); background-attachment: fixed; background-size: cover; background-position: center; width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important;">
-				<div class="container" style="padding-left: 50px; padding-right: 0px; margin-left: -100px; margin-right: unset; width: 50% !important; height: 100% !important; background: #232323; border-radius: 0px 400px 400px 0px; border-right: 300px; max-width: 1280px !important;">
+			<section class="hero-sec" style="border-right: solid 100px #232323; background-image: url('http://localhost:8080/wp-content/uploads/2023/07/slide-3.jpg'); background-attachment: fixed; background-size: cover; background-position: center; width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important;">
+				<div class="container" style="padding-left: 0px; padding-right: 0px; margin-left: -100px; margin-right: unset; width: 50% !important; height: 100% !important; background: #232323; border-radius: 0px 400px 400px 0px; border-right: 300px; max-width: 1280px !important;">
 					<div class="row">
-						<div class="content-sec">
+						<div class="content-sec" style="right: 50px;">
 							<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-1.png" width="160">
 							<h2 class="section-title" style="font-size: 38px; text-shadow: 3px 3px 20px rgba(0,0,0,0.75);"><?php echo esc_html($techup_banner_title); ?>
 							</h2>
@@ -188,10 +188,10 @@ if($techup_enable_banner_section==true ) { ?>
 
 
 		<div data-slidr="two" style="min-width: 100vw; min-height: 50vh;">
-		<section class="hero-sec" style="background-image: url('http://localhost:8080/wp-content/uploads/2023/07/slide-3.jpg'); background-attachment: fixed; background-size: cover; background-position: center; width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important;">
-				<div class="container" style="padding-left: 50px; padding-right: 0px; margin-left: -100px; margin-right: unset; width: 50% !important; height: 100% !important; background: #232323; border-radius: 0px 400px 400px 0px; border-right: 300px; max-width: 1280px !important;">
+		<section class="hero-sec" style="border-right: solid 100px #232323; background-image: url('http://localhost:8080/wp-content/uploads/2023/07/slide-3.jpg'); background-attachment: fixed; background-size: cover; background-position: center; width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important;">
+				<div class="container" style="padding-left: 0px; padding-right: 0px; margin-left: -100px; margin-right: unset; width: 50% !important; height: 100% !important; background: #232323; border-radius: 0px 400px 400px 0px; border-right: 300px; max-width: 1280px !important;">
 					<div class="row">
-						<div class="content-sec">
+						<div class="content-sec" style="right: 50px;">
 						<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-1.png" width="160">
 							<h2 class="section-title" style="font-size: 38px; text-shadow: 3px 3px 20px rgba(0,0,0,0.75);">Australian Agricultural Drone Technology.</h2>
 
@@ -209,10 +209,10 @@ if($techup_enable_banner_section==true ) { ?>
 
 
 		<div data-slidr="three" style="min-width: 100vw; min-height: 50vh;">
-			<section class="hero-sec" style="background-image: url('http://localhost:8080/wp-content/uploads/2023/07/slide-3.jpg'); background-attachment: fixed; background-size: cover; background-position: center; width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important;">
-				<div class="container" style="padding-left: 50px; padding-right: 0px; margin-left: -100px; margin-right: unset; width: 50% !important; height: 100% !important; background: #232323; border-radius: 0px 400px 400px 0px; border-right: 300px; max-width: 1280px !important;">
+			<section class="hero-sec" style="border-right: solid 100px #232323; background-image: url('http://localhost:8080/wp-content/uploads/2023/07/slide-3.jpg'); background-attachment: fixed; background-size: cover; background-position: center; width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important;">
+				<div class="container" style="padding-left: 0px; padding-right: 0px; margin-left: -100px; margin-right: unset; width: 50% !important; height: 100% !important; background: #232323; border-radius: 0px 400px 400px 0px; border-right: 300px; max-width: 1280px !important;">
 					<div class="row">
-						<div class="content-sec">
+						<div class="content-sec" style="right: 50px;">
 						<img src="http://localhost:8080/wp-content/uploads/2023/07/logo-white-1.png" width="160">
 							<h2 class="section-title" style="font-size: 38px; text-shadow: 3px 3px 20px rgba(0,0,0,0.75);">Spraying, Seeding & Surveying Drones.</h2>
 
