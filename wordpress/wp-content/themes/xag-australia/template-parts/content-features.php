86677e7b4ccfae4fe3cbe15aa0ff58cc9bfbbad2
XAG-Australia <david@thesydney.agency>
2023-07-24 17:41:00 +1000

    new icon stuff

--- wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
@@ -26,7 +26,7 @@ if($techup_enable_features_section==true ) {
 		?> 
         <div class="col-lg-4 col-md-6 col-sm-12" style="width: 100%; padding-left: 0px !important; ">
           <div class="feature-box feature-box-one feature-box-1" style="width: 420px; background-color: #ffffff !important; max-width: 420px; height: 200px; border-radius: 0px; margin-left: auto; margin-right: auto;">
-            <div class="icon" style="background: url('http://localhost:8080/wp-content/uploads/2023/07/icon-1A.png') no-repeat #ffffff; background-size: 100px; background-position: center; margin-bottom: 30px; transition: all 0.3s ease-in-out;"></div>
+            <div class="icon" style="background: url('http://localhost:8080/wp-content/uploads/2023/07/icon-1A.png') no-repeat #ffffff; background-size: 100px; background-position: center;  transition: all 0.3s ease-in-out;"></div>
             <div class="content">
               <h5 class="title-box" style="color: #8d8d8d !important;">AERIAL SPRAYING</a></h5>
               <p style="display: none; text-align: left; color: #d0d0d0; margin-top: 40px;">Spray safely and efficiently with XAG agricultural drones.</p>
@@ -35,7 +35,7 @@ if($techup_enable_features_section==true ) {
         </div>
         <div class="col-lg-4 col-md-6 col-sm-12" style="width: 100%; padding-left: 0px !important;">
           <div class="feature-box feature-box-one feature-box-2" style="width: 420px; background-color: #ffffff !important; max-width: 420px; height: 200px; border-radius: 0px; margin-left: auto; margin-right: auto;"">
-            <div class="icon" style="display: none; background: url('http://localhost:8080/wp-content/uploads/2023/07/service-2.png') no-repeat #ffffff; background-size: 110px; background-position: center; margin-bottom: 30px; transition: all 0.3s ease-in-out;"></div>
+            <div class="icon" style="background: url('http://localhost:8080/wp-content/uploads/2023/07/service-2.png') no-repeat #ffffff; background-size: 100px; background-position: center;  transition: all 0.3s ease-in-out;"></div>
             <div class="content">
             <h5 class="title-box" style="display: none; color: #8d8d8d !important; font-size: 14px;">UNMANNED GROUND VEHICLES</a></h5>
               <p style="display: none; text-align: left; color: #d0d0d0; margin-top: 40px;">Precision plant services, spreading systems, AI mowing and unmanned transportation.</p>
@@ -44,7 +44,7 @@ if($techup_enable_features_section==true ) {
         </div>
         <div class="col-lg-4 col-md-6 col-sm-12" style="width: 100%; padding-left: 0px !important;">
           <div class="feature-box feature-box-one feature-box-3" style="width: 420px; background-color: #ffffff !important; max-width: 420px; height: 200px; border-radius: 0px; margin-left: auto; margin-right: auto;">
-            <div class="icon" style="display: none; background: url('http://localhost:8080/wp-content/uploads/2023/07/service-3.png') no-repeat #ffffff; background-size: 110px; background-position: center; margin-bottom: 30px; transition: all 0.3s ease-in-out;"></div>
+            <div class="icon" style="display: none; background: url('http://localhost:8080/wp-content/uploads/2023/07/icon-3A.png') no-repeat #ffffff; background-size: 100px; background-position: center; transition: all 0.3s ease-in-out;"></div>
             <div class="content">
             <h5 class="title-box" style="display: none; color: #8d8d8d !important;">AERIAL SURVEYING</a></h5>
             <p style="display: none; text-align: left; color: #d0d0d0; margin-top: 40px;">Survey high-resolution and detailed models of any terrain.</p>
