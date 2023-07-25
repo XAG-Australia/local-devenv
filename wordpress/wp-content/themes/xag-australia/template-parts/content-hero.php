77428b93de7289756c3fe92deedcee7105c37ffe
XAG-Australia <david@thesydney.agency>
2023-07-25 18:51:48 +1000

    new hero 1

--- wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-hero.php
@@ -166,7 +166,7 @@ if($techup_enable_banner_section==true ) { ?>
 		<div data-slidr="one" style="min-width: 100vw; min-height: 65vh;">
 			<section class="hero-sec" style="width: 1280px; margin-left: auto; margin-right: auto; padding: 0px !important; ">
 					<video autoplay muted loop style="width: 100%; height: 100%; position: absolute; object-fit: cover;">
-					<source src="http://localhost:8080/wp-content/uploads/2023/07/hero-1A.mp4" type="video/mp4" />
+					<source src="http://localhost:8080/wp-content/uploads/2023/07/hero-1B.mp4" type="video/mp4" />
 					</video>
 				<div class="container" style="position: absolute; z-index: 99; padding-left: 0px; padding-right: 0px; margin-left: unset; margin-right: unset; width: 30% !important; height: calc(100% - 180px) !important; background: #c01920; border-radius: 0px 400px 400px 0px; max-width: 1280px !important; margin-top: 180px !important; box-shadow: 3px 0px 20px rgba(0,0,0,0.35);">
 					<div class="row">
@@ -246,7 +246,7 @@ if($techup_enable_banner_section==true ) { ?>
 	 fade: true,
 	 controls: 'border',
 	 theme: '#ffffff',
-	 timing: { 'fade': '1s ease-in-out' },
+	 timing: { 'fade': '1000ms ease-in-out' },
 	 transition: 'fade'
 	  
 });
@@ -256,7 +256,7 @@ if($techup_enable_banner_section==true ) { ?>
 s.add('h', ['one', 'two', 'three', 'one']);
 
 // Now start.
-s.auto(13000, 'right', 'one');
+s.auto(6000, 'right', 'one');
 
 	function auto(opt_msec, opt_direction, opt_start) {};
 
