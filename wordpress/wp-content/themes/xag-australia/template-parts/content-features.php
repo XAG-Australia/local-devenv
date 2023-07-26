e8b0efe25ceae55cb85d47f35eb61bdedb0f19a0
XAG-Australia <david@thesydney.agency>
2023-07-26 10:33:31 +1000

    Simple Updates/New Hero 2

--- wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
+++ wordpress/wp-content/themes/xag-australia/template-parts/content-features.php
@@ -17,7 +17,7 @@ if($techup_enable_features_section==true ) {
 ?>
 <div class="feature-sec buco" style="height: 35vh; padding-top: 10px !important; margin-bottom: 10px; padding: 0px; background: #f7f7f7; background-attachment: fixed; background-size: cover; background-position: center;">
     <div class="container-fluid" style="width: 100%; max-width: 1280px; padding-left: 0px; padding-right: 0px; height: 325px;">
-    <h2 class="section-title" style="font-size: 18px; text-align: center; color: #d0d0d0; font-weight: 300 !important; font-family: 'Open Sans', sans-serif; letter-spacing: 3px; margin-bottom: 25px; margin-top: 25px; width: 1280px; margin-left: auto; margin-right: auto;">Pioneering aerial, and unmanned ground <b style="color:#c01920;">drone technology.</b></h2>
+    <h2 class="section-title" style="font-size: 22px; text-align: left; color: #d0d0d0; font-weight: 300 !important; font-family: 'Roboto Condensed', sans-serif; letter-spacing: 4px; margin-bottom: 35px; margin-top: 35px; width: 1280px; margin-left: auto; margin-right: auto;">Pioneering aerial, and unmanned ground <b style="color:#232323;">drone technology.</b></h2>
       <div class="row" style="width: 100%; margin-left: auto; margin-right: auto;">
 		<?php
 		$count = 0;
@@ -62,7 +62,7 @@ if($techup_enable_features_section==true ) {
     </div>
     </div>
   <div class="feature-sec" style="height: calc(75vh - 180px); width: 1280px; border: 0px !important; margin-left: auto; margin-right: auto; ">
-  <h2 class="section-title" style="font-size: 32px; text-align: center; color: #d0d0d0; font-weight: 300 !important; font-family: 'Open Sans', sans-serif !important; letter-spacing: 16px; width: 1280px; margin-left: auto; margin-right: auto; margin-top: 50px; margin-bottom: 50px;">FEATURED</h2>
+  <h2 class="section-title" style="font-size: 32px; text-align: left; color: #d0d0d0; font-weight: 300 !important; font-family: 'Open Sans', sans-serif !important; letter-spacing: 16px; width: 1280px; margin-left: auto; margin-right: auto; margin-top: 50px; margin-bottom: 50px;">FEATURED</h2>
       <div class="featured-1" style="height: 420px; width: 420px;  float: left; background-image: url('http://localhost:8080/wp-content/uploads/2023/07/product-1.png'); background-color: rgb(250 250 250 / 75%); background-blend-mode: lighten; background-size: cover; background-position: bottom; transition: all 0.5s ease-in-out;">
       <h2 class="section-title" style="opacity: 1; font-size: 38px; text-align: center; margin-top: 50px;">P100 PRO</h2>
       <div class="btn-wraper product" style="opacity: 0; padding-top: 50%; transition: all 0.4s ease-in-out;">
